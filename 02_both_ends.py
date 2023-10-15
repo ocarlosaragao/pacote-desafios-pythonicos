e13a9c6d0867bc2f30a9d8d5b86468b2d5e3fe42
ocarlosaragao <105019027+ocarlosaragao@users.noreply.github.com>
2023-10-15 15:51:59 +0000

    02_both_ends.py resolvido

--- 02_both_ends.py
+++ 02_both_ends.py
@@ -8,8 +8,12 @@ for menor que 2, retorne uma string vazia.
 """
 
 def both_ends(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+    if len(s) < 2:
+        return ''
+    else:
+        new_s = s[0] + s[1] + s[-2] + s[-1]
+        return new_s
+
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
